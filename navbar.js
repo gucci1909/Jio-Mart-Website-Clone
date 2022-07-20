a318df1404b902cda8b103bf98a7cc8f693741fb
vivekanjan12345 <vivekanjankc12345@gmail.com>
2022-07-20 18:27:34 +0530

    done

---
+++ navbar.js
@@ -0,0 +1,8 @@
+document.querySelector("form").addEventListener("submit",myserch);
+function myserch(event)
+{
+    event.preventDefault();
+    let query=document.getElementById("query").value;
+    console.log(query);
+    
+}
