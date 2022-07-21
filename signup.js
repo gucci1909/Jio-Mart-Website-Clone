689ffa4eb049c8d90288c823b06b23acd96314cf
Amardeep <amardeepsingh1024@gmail.com>
2022-07-21 20:02:16 +0530

    signup updated

--- signup.js
+++ signup.js
@@ -1,13 +1,27 @@
-let btn=document.getElementById("button")
-btn.addEventListener("click", loginVerify)
-
 let OTP=document.getElementById("login_OTP")
 
 let loginVerify=()=>{
     if(OTP.value==890627){
-        return;
+        let firstName=document.getElementById("first_name").value
+        let lastName=document.getElementById("last_name").value
+        let email=document.getElementById("login_email").value
+        let password=document.getElementById("login_password").value
+        let password2=document.getElementById("login_password_check").value
+        if(firstName=="" || lastName=="" || email=="" || password=="" || password2==""){
+            alert("Please fill in all the fields")
+        }else{
+            if(password==password2){
+                window.location.href="./index.html"
+            }else{
+                let msg2=document.getElementById("txt4")
+                msg2.innerText="Confirm password you entered does not match to your original password"
+            }
+        }
     }else{
         let msg=document.getElementById("OTP_verify")
         msg.innerText="Wrong OTP Entered"
     }
 }
+
+let btn=document.getElementById("button")
+btn.addEventListener("click", loginVerify)
