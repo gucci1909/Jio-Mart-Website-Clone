a51900792bd988a1eeb7466552f5cf31be3c035a
gucci1909 <umangarora0134@gmail.com>
2022-07-21 18:56:12 +0530

    Thursday Work

---
+++ js/Product2.js
@@ -0,0 +1,379 @@
+// var Product2 = [
+//     {
+//         image_url: "https://m.media-amazon.com/images/I/71jHO2zTrnL._AC_SR255,340_.jpg",
+//         name : "Future Icon 3-Bar Tee",
+//         discount : "25%",
+//         price : 210,
+//         price1 : 330,
+//         category : "T-shirt",
+//         color : "Pink"
+
+//     },
+//     {
+//         image_url: "https://m.media-amazon.com/images/I/71Juw8eDAsL._AC_SR255,340_.jpg",
+//         name : "Heathered 3G Tank",
+//         discount : "35%",
+//         price : 555,
+//         price1: 770,
+//         category : "Tank Top",
+//         color : "Blue"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/71ETawgdEIL._AC_SR255,340_.jpg",
+//         name : "Badge of Sport Tee",
+//         discount : "12%",
+//         price : 225,
+//         price1 : 310,
+//         category : "T-shirt",
+//         color : "Black"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/71QK+PGV22L._AC_SR255,340_.jpg",
+//         name : "Tiger Camo Tee",
+//         discount : "20%",
+//         price : 435,
+//         price1 : 660,
+//         category : "Tank Top",
+//         color : "White"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/81eyb5jVroS._AC_SR255,340_.jpg",
+//         name : "Beefy-T Long Sleeve Henley",
+//         discount : "30%",
+//         price : 440,
+//         price1 : 770,
+
+//         category : "Activewear shirts",
+//         color : "Black"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/71sbI515BZS._AC_SR255,340_.jpg",
+//         name : "Beefy-T Long Sleeve Henley",
+//         discount : "20%",
+//         price : 540,
+//         price1 : 770,
+//         category : "Activewear shirts",
+//         color : "Red"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/71dprCNVIiL._AC_SR255,340_.jpg",
+//         name : "Sport Men's Performance Quarter-Zip Pullover",
+//         discount : "30%",
+//         price : 225,
+//         price1 : 345,
+//         category : "Activewear shirts",
+//         color : "Grey"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/71igqfdI6mL._AC_SR255,340_.jpg",
+//         name : "Crayon Wave Short Sleeve Tee",
+//         discount : "40%",
+//         price :318 ,
+//         price1: 550 ,
+
+//         category : "Tank Top",
+//         color : "Blue"
+        
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/71FIah-lZ9L._AC_SR255,340_.jpg",
+//         name : "Billy's Burgers Short Sleeve Tee",
+//         discount : "25%",
+//         price : 327,
+//         price1 : 440,
+//         category : "T-shirt",
+//         color : "White"
+
+//     },
+//     {
+//         image_url: "https://m.media-amazon.com/images/I/81eQdSgMFiL._AC_SR255,340_.jpg",
+        
+//         name : "Bahama Coast Paradiso",
+//         discount : "20%",
+//         price :555 ,
+//         price1 : 669,
+//         category : "Activewear shirts",
+//         color : "Blue"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/81rM+ppd0VL._AC_SR255,340_.jpg",
+        
+//         name : "On the Road Long Sleeve Tee",
+//         discount : "30%",
+//         price :470 ,
+//         price1 : 567,
+//         category : "Activewear shirts",
+//         color : "Grey"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/818P0LgvweL._AC_SR255,340_.jpg",
+        
+//         name : "On the Road Long Sleeve Tee",
+//         discount : "20%",
+//         price :428 ,
+//         price1 : 536,
+//         category : "Tank Top",
+//         color : "Blue"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/71YKqYLNA7L._AC_SR255,340_.jpg",
+        
+//         name : "Gold Lines",
+//         discount : "30%",
+//         price : 435,
+//         price1 : 578,
+//         category : "T-shirts",
+//         color : "Black"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/71JpjPGZrpL._AC_SR255,340_.jpg",
+        
+//         name : "Comp Logo Short Sleeve Tee",
+//         discount : "23%",
+//         price : 123 ,
+//         price1 : 189,
+//         category : "T-shirts",
+//         color : "White"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/71Tm7QTY6lL._AC_SR255,340_.jpg",
+        
+//         name : "The Stretch-Cotton Shirt",
+//         discount : "32%",
+//         price :284,
+//         price1 : 345,
+//         category : "Activewear shirts",
+//         color : "Grey"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/818yPP9f1aL._AC_SR255,340_.jpg",
+        
+//         name : "Ainsley",
+//         discount : "20%",
+//         price : 240,
+//         price1 : 440,
+//         category : "T-shirts",
+//         color : "Blue"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/71H+Rny-ZnL._AC_SR255,340_.jpg",
+//         name : "Retro Frog",
+//         discount : "30%",
+//         price : 445,
+//         price1 : 555,
+
+//         category : "Activewear shirts",
+//         color : "Grey"
+
+//     },{
+//         image_url: "https://m.media-amazon.com/images/I/71bPeBIrKAL._AC_SR255,340_.jpg",
+//         name : "Sunrise V12",
+//         discount : "20%",
+//         price : 425,
+//         price1 : 660,
+//         category : "Activewear shirts",
+//         color : "Black"
+
+//     },
+//     {
+//         image_url: "https://m.media-amazon.com/images/I/71feLIUWzyL._AC_SR255,340_.jpg",
+//         name : "Gradient B21",
+//         discount : "35%",
+//         price : 323,
+//         price1 : 240,
+//         category : "T-shirts",
+//         color : "Red"
+
+//     }
+// ]
+
+let search = async() =>{
+    let res = await fetch(`https://umang-api.herokuapp.com/product2`);
+    let data = await res.json();
+    console.log(data.Product2)
+    display(data.Product2);
+    return data.Product2;  
+  }
+  search()
+  function display(data)
+    {
+      let container = document.getElementById("products") 
+      container.innerHTML=null;
+      let h6 = document.createElement("p");
+      h6.innerText="ALL PRODUCTS"
+     
+      data.forEach(function (elem)
+      {
+        let h1 = document.createElement("h1");
+        h1.innerText = `${elem.discount}`;
+        h1.setAttribute("class","h1");
+        let h2 = document.createElement("h1");
+        h2.innerText = "OFF";
+        h2.setAttribute("class" , "h2");
+        let img1 = document.createElement("img");
+        img1.setAttribute("src","https://www.jiomart.com/assets/version1657814599/smartweb/images/icons/offer_bg.svg")
+        img1.setAttribute("class","img1");
+        let div = document.createElement("div");
+        div.className="class"
+        let img = document.createElement("img");
+        img.src = elem.image_url;
+        img.className="image";
+        let  name = document.createElement("h3");
+        name.innerText=elem.name;
+        name.className="name";
+        let p1 = document.createElement("p");
+        p1.innerText= "M.R.P";
+        p1.className="pr"
+        let cprice = document.createElement("p");
+        cprice.innerHTML = `&#8377`;
+        cprice.setAttribute("class","pricing")
+        let ccprice = document.createElement("p");
+        ccprice.innerHTML = elem.price1;
+        ccprice.setAttribute("class","pricing1")
+        let rupees = document.createElement("p");
+        rupees.innerHTML="&#8377";
+        rupees.className="pricing2";
+        let price = document.createElement("p");
+        price.innerHTML= `${elem.price}.00`;
+        price.className="pricing3";
+        let button = document.createElement("button");
+        button.className= "button"
+        button.innerText = "ADD TO CART";
+        button.addEventListener("click",clc)
+        button.addEventListener("click",function(){
+          addcarfun(elem)
+      })
+        div.append(h1,h2,img1,img,name,p1,cprice,ccprice,rupees,price,button);
+        container.append(div)
+        
+  
+      })
+  
+  }
+  // display(Product2)
+  
+   let ssn = async() =>
+  {
+    let Product2 = await search()
+    
+     Product2.sort(function (a,b)
+    {
+      return b.price - a.price;
+    })
+    display(Product2)
+  
+  }
+  let bbn = async() =>
+  {
+    let Product2 = await search()
+   Product2.sort(function (a,b)
+     {
+      return a.price - b.price;
+  
+     })
+    display(Product2)
+  
+  }
+  let Peach = async() =>
+  {
+    let Product2 = await search()
+    var filterrole = Product2.filter((ele) =>{
+    return ele.category == "T-shirts";
+    })
+    display(filterrole);
+  
+  }
+  let Plum = async() =>
+  {
+    let Product2 = await search()
+    let filterrole = Product2.filter((ele) =>
+    {
+      return ele.category == "Activewear shirts";
+    })
+    display(filterrole);
+  
+  }
+  let Avocado = async() =>
+  {
+    let Product2 = await search()
+    let filterrole = Product2.filter((ele) =>
+    {
+      return ele.category == "Tank Top";
+    })
+    display(filterrole);
+  
+  }
+  let Nectarine = async() =>
+  {
+    let Product2 = await search()
+    let filterrole = Product2.filter((ele) =>
+    {
+      return ele.color == "Red";
+    })
+    display(filterrole);
+  
+  }
+  
+  let Black = async() =>
+  {
+    let Product2 = await search()
+    let filterrole = Product2.filter((ele) =>
+    {
+      return ele.color == "Black";
+    })
+    display(filterrole);
+  
+  }
+  let Grey = async() =>
+  {
+    let Product2 = await search()
+    let filterrole = Product2.filter((ele) =>
+    {
+      return ele.color == "Grey";
+    })
+    display(filterrole);
+  
+  }
+  let Discount = async() =>
+  {
+    let Product2 = await search()
+    window.location.reload()
+    display(Product2);
+   
+  }
+  let allP = async() =>{
+    let Product2 = await search()
+      window.location.reload()
+      display(Product2)
+  }
+  let Stock = async()=>{
+    let Product2 = await search()
+      let filterrole = Product2.filter((ele) =>
+    {
+      return ele.Stock == "Yes";
+    })
+    display(filterrole);
+  }
+  
+  let count = localStorage.getItem("counter") || 0;
+  function clc()
+  {
+    count++;
+    localStorage.setItem("counter" , count);
+    if(count==1){
+      alert(`${count} item has been added to cart`)
+  
+  
+    }
+    else{
+      alert(`${count} items has been added to cart`)
+  
+    }
+  }
+  let cart=JSON.parse(localStorage.getItem("cart"))  || []
+  let addcarfun = (ele)=>
+  {
+    cart.push(ele)
+    localStorage.setItem("cart",JSON.stringify(cart))
+  }
+  
+  
