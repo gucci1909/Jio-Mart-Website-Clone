e674d91c59d50f102c815a22ffce1d1e1b832695
Amardeep <amardeepsingh1024@gmail.com>
2022-07-23 17:39:22 +0530

    signup functionality added

--- signup.js
+++ signup.js
@@ -1,3 +1,6 @@
+let loginData=JSON.parse(localStorage.getItem("loginData")) || []
+let loginNumber=localStorage.getItem("loginNumber")
+
 let OTP=document.getElementById("login_OTP")
 
 let loginVerify=()=>{
@@ -10,8 +13,18 @@ let loginVerify=()=>{
         if(firstName=="" || lastName=="" || email=="" || password=="" || password2==""){
             alert("Please fill in all the fields")
         }else{
-            if(password==password2){
-                window.location.href="./index.html"
+            if(password==password2){                                   // Login Data saving process starts here
+                let person=new User(firstName, lastName, email, loginNumber, password)
+                if(loginData.length==0){
+                    loginData.push(person)
+                    localStorage.setItem("loginData", JSON.stringify(loginData))
+                }else{
+                    loginData.forEach((elem)=>{
+                        elem.loginStatus=false
+                    })
+                    loginData.push(person)
+                    localStorage.setItem("loginData", JSON.stringify(loginData))
+                }
             }else{
                 let msg2=document.getElementById("txt4")
                 msg2.innerText="Confirm password you entered does not match to your original password"
@@ -25,3 +38,15 @@ let loginVerify=()=>{
 
 let btn=document.getElementById("button")
 btn.addEventListener("click", loginVerify)
+
+
+class User{
+    constructor(fname, lname, email, number, password){
+        this.firstName=fname;
+        this.lastName=lname;
+        this.loginEmail=email;
+        this.loginNumber=number;
+        this.loginPassword=password;
+        this.loginStatus=true;
+    }
+}
